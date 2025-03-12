e8d7e6f3361771ff8c133c4177e3cfca7554b89f
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-13 02:29:57 +0900

    main 생성

---
+++ server/src/main/java/takeoff/logistics_service/msa/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package takeoff.logistics_service.msa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/takeoff/logistics_service/msa/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package takeoff.logistics_service.msa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
