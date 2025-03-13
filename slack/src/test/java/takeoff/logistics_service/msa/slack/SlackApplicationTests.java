5b47b4c2114311f4e791e70474cb1d36c54bc0b1
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-14 02:34:11 +0900

    feature/#6 slack C,R 로직 구현

---
+++ slack/src/test/java/takeoff/logistics_service/msa/slack/SlackApplicationTests.java
@@ -0,0 +1,13 @@
+package takeoff.logistics_service.msa.slack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
