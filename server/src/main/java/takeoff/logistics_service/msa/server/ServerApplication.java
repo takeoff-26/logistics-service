790e41395f45275c089be0d8b706a6b597f9e8bd
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-13 14:26:33 +0900

    feature/#4 환경세팅

--- server/src/main/java/takeoff/logistics_service/msa/server/ServerApplication.java
+++ server/src/main/java/takeoff/logistics_service/msa/server/ServerApplication.java
@@ -2,8 +2,10 @@ package takeoff.logistics_service.msa.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
