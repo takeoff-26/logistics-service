80da8e6c68df411d1dc18bad66a247cae47c1c21
hyezuu <duudu0704@gmail.com>
2025-03-15 06:23:14 +0900

    feature/10: Stock Application 구현

---
+++ product/src/main/java/takeoff/logistics_service/msa/product/stock/application/service/StockService.java
@@ -0,0 +1,29 @@
+package takeoff.logistics_service.msa.product.stock.application.service;
+
+import takeoff.logistics_service.msa.product.stock.presentation.dto.StockIdDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.request.AbortStockRequestDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.request.DecreaseStockRequestDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.request.IncreaseStockRequestDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.request.PostStockRequestDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.request.PrepareStockRequestDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.response.DecreaseStockResponseDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.response.GetStockResponseDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.response.IncreaseStockResponseDto;
+import takeoff.logistics_service.msa.product.stock.presentation.dto.response.PostStockResponseDto;
+
+public interface StockService {
+
+	PostStockResponseDto saveStock(PostStockRequestDto requestDto);
+
+	IncreaseStockResponseDto increaseStock(IncreaseStockRequestDto requestDto);
+
+	DecreaseStockResponseDto decreaseStock(DecreaseStockRequestDto requestDto);
+
+	void delete(StockIdDto stockIdDto);
+
+	GetStockResponseDto findStock(StockIdDto stockIdDto);
+
+	void prepareStock(PrepareStockRequestDto requestDto);
+
+	void abortStock(AbortStockRequestDto requestDto);
+}
