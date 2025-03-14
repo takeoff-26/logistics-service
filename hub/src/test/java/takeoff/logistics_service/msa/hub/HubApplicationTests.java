26f8ca389292e0292fd3ab190bcf157fe2a01e45
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-15 01:59:56 +0900

    feature/#18 hub save 생성

---
+++ hub/src/test/java/takeoff/logistics_service/msa/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package takeoff.logistics_service.msa.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
