4e06fed3838b52f0220bc9914e989848917c0c52
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-14 15:25:31 +0900

    feature/#14 Slack Search 로직 작성

--- slack/src/main/java/takeoff/logistics_service/msa/slack/infrastructure/persistence/impl/JpaSlackRepositoryCustomImpl.java
+++ slack/src/main/java/takeoff/logistics_service/msa/slack/infrastructure/persistence/impl/JpaSlackRepositoryCustomImpl.java
@@ -2,14 +2,21 @@ package takeoff.logistics_service.msa.slack.infrastructure.persistence.impl;
 
 import static takeoff.logistics_service.msa.slack.model.entity.QSlack.slack;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import takeoff.logistics_service.msa.slack.infrastructure.persistence.JpaSlackRepositoryCustom;
 import takeoff.logistics_service.msa.slack.model.entity.QSlack;
+import takeoff.logistics_service.msa.slack.model.entity.Slack;
 import takeoff.logistics_service.msa.slack.presentation.dto.request.SlackRequestDto;
 import takeoff.logistics_service.msa.slack.presentation.dto.response.SlackResponseDto;
 
@@ -27,15 +34,62 @@ public class JpaSlackRepositoryCustomImpl implements JpaSlackRepositoryCustom {
     @Override
     public Page<SlackResponseDto> searchSlack(SlackRequestDto slackRequestDto, Pageable pageable) {
 
-        queryFactory.select(slack)
+        List<Slack> fetch = queryFactory.select(slack)
             .from(slack)
             .where(
                 containsMessage(slackRequestDto.contentsRequestDto().message())
-                createBeforeTimeAndAfterTime()
             )
+            //Auditor 생성시 변경해야함.
+            .orderBy(slack.contents.sent_At.asc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long totalCount = queryFactory.select(slack.count())
+            .from(slack)
+            .where(
+                containsMessage(slackRequestDto.contentsRequestDto().message())
+            )
+            .fetchOne();
+
+        if (totalCount == null) totalCount = 0L;
+
+        List<SlackResponseDto> responseDtoList = fetch.stream()
+            .map(SlackResponseDto::from)
+            .toList();
+
+
+        return new PageImpl<>(responseDtoList, pageable, totalCount);
     }
 
     private BooleanExpression containsMessage(String message) {
         return message == null ? null : slack.contents.message.containsIgnoreCase(message);
     }
+
+    //Auditor 구성시 정렬 추가
+//    private List<OrderSpecifier<?>> dynamicOrder(Pageable pageable) {
+//        List<OrderSpecifier<?>> orderSpecifierList = new ArrayList<>();
+//
+//        if (pageable.getSort() != null) {
+//            for (Sort.Order sortOrder : pageable.getSort()) {
+//                Order direction = sortOrder.isAscending() ? Order.ASC : Order.DESC;
+//
+//                switch (sortOrder.getProperty()) {
+//                    case "createdAt":
+//                        orderSpecifierList.add(new OrderSpecifier<>(direction, slack.createdAt));
+//                        break;
+//                    case "updatedAt":
+//                        orderSpecifierList.add(new OrderSpecifier<>(direction, slack.updatedAt));
+//                        break;
+//                    default:
+//                        throw new IllegalArgumentException(
+//                            "잘못된 정렬 필드입니다. : " + sortOrder.getProperty());
+//                }
+//            }
+//        } else {
+//            orderSpecifierList.add(new OrderSpecifier<>(Order.ASC, slack.createdAt));
+//        }
+//        return orderSpecifierList;
+//
+//    }
 }
