868b1916ae684b65dcd44f583335a6173966aaab
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-17 02:55:50 +0900

    feature/#26 common 추가

--- slack/src/main/java/takeoff/logistics_service/msa/slack/SlackApplication.java
+++ slack/src/main/java/takeoff/logistics_service/msa/slack/SlackApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {"takeoff.logistics_service.msa.common","takeoff.logistics_service.msa.slack"}
+)
 @EnableFeignClients
 public class SlackApplication {
 
