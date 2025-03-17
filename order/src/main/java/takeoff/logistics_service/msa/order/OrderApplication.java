cbd004a0c23366de8634416fafe207f137121d1a
sodychoe <sodychoe@icloud.com>
2025-03-17 16:09:06 +0900

    feature/#35 : 공통모듈 임포트 설정 추가

--- order/src/main/java/takeoff/logistics_service/msa/order/OrderApplication.java
+++ order/src/main/java/takeoff/logistics_service/msa/order/OrderApplication.java
@@ -2,8 +2,10 @@ package takeoff.logistics_service.msa.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"takeoff.logistics_service.msa.common", "takeoff.logistics_service.msa.order"})
 public class OrderApplication {
 
   public static void main(String[] args) {
