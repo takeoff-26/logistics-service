aac216a77d1a00fc13d3a439ea8390d15fee80f3
sodychoe <sodychoe@icloud.com>
2025-03-17 11:03:18 +0900

    feature/#7 application 계층 구현

---
+++ delivery/src/main/java/takeoff/logisticsservice/msa/delivery/application/DeliveryService.java
@@ -0,0 +1,28 @@
+package takeoff.logisticsservice.msa.delivery.application;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import takeoff.logisticsservice.msa.delivery.model.entity.Delivery;
+import takeoff.logisticsservice.msa.delivery.model.repository.DeliveryRepository;
+import takeoff.logisticsservice.msa.delivery.presentation.dto.PostDeliveryRequestDto;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryService {
+
+  private final DeliveryRepository deliveryRepository;
+
+  // TODO : 분산 트랜잭션 
+  @Transactional
+  public UUID saveDelivery(PostDeliveryRequestDto dto) {
+    Delivery delivery = Delivery.builder()
+        .orderId(dto.getOrderId())
+        .build();
+
+    deliveryRepository.save(delivery);
+
+    return delivery.getDeliveryId().getId();
+  }
+}
