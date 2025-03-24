d8a3594bf627f40665bf7d851b1dca9dac927ccf
sodychoe <sodychoe@icloud.com>
2025-03-24 20:34:56 +0900

    fix/#128 : 커밋 꼬여서 반영 안된거 수정

--- delivery/src/main/java/takeoff/logisticsservice/msa/delivery/delivery/presentation/internal/DeliveryInternalController.java
+++ delivery/src/main/java/takeoff/logisticsservice/msa/delivery/delivery/presentation/internal/DeliveryInternalController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import takeoff.logistics_service.msa.common.annotation.RoleCheck;
 import takeoff.logistics_service.msa.common.domain.UserInfo;
@@ -26,7 +27,7 @@ public class DeliveryInternalController {
   private final DeliveryService deliveryService;
 
   @GetMapping
-  public List<UUID> findAllDeliveryIdByUser(Long userId) {
+  public List<UUID> findAllDeliveryIdByUser(@RequestParam(name = "userId") Long userId) {
     return deliveryService.findAllDeliveryIdByUser(userId);
   }
 
