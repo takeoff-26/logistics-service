c6ae76d49d86e2393ee10d777badf56cdc586116
sodychoe <sodychoe@icloud.com>
2025-03-24 22:34:13 +0900

    docs/#128 : 라우팅 추가

--- delivery/src/main/java/takeoff/logisticsservice/msa/delivery/delivery/application/client/UserClient.java
+++ delivery/src/main/java/takeoff/logisticsservice/msa/delivery/delivery/application/client/UserClient.java
@@ -2,12 +2,15 @@ package takeoff.logisticsservice.msa.delivery.delivery.application.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import takeoff.logisticsservice.msa.delivery.delivery.application.client.dto.response.GetUserResponseDto;
 
 @Component
 @FeignClient(name = "userDeliveryInternal")
 public interface UserClient {
 
-  GetUserResponseDto findByUserId(Long userId);
+  @GetMapping
+  GetUserResponseDto findByUserId(@RequestParam("userId") Long userId);
 
 }
